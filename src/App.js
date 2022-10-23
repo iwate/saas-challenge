01f464354e2145f77713d0f5a26e1eace79e4034
iwate <github@iwate.me>
2022-10-23 15:21:25 +0900

    Commit made from VS Code Azure Static Web Apps

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import useSWR from 'swr'
+
+const fetcher = (...args) => fetch(...args).then(res => res.text());
+
+function useBackend (name) {
+  const { data, error } = useSWR(`/api/HttpTrigger1?name=${name}`, fetcher)
+
+  return {
+    message: data,
+    isLoading: !error && !data,
+    isError: error
+  }
+}
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  const data = useBackend('iwate');
+
+  if (data.isLoading)
+    return <div>Now Loading...</div>;
+  
+  if (data.isError)
+    return <div>An error occurred.</div>
+  
+  return <div>Hello {data.message}</div>;
 }
 
 export default App;
